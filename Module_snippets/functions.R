598bc8dcb874608ce589a8d5876e7cc92814b92d
Jan Stanstrup <stanstrup@gmail.com>
2016-07-14 17:03:24 +0200

    barplot of contaminants

--- Module_snippets/functions.R
+++ Module_snippets/functions.R
@@ -65,3 +65,23 @@ get_cont_list <- function(polarity,type) {
     
     return(out)
 }
+
+
+
+
+# gg contamination barplot to plotly --------------------------------------
+# with fixed tooltip
+
+cont_screen_gg2plotly <- function(x){
+    require(magrittr)
+    require(dplyr)
+    require(plotly)
+    
+    gg <- plotly_build(x)
+    
+    gg$data[[1]]$text <- paste("Compound:",   x %>% extract2("data") %>% arrange(desc(EIC_median)) %>% extract2("comp_name") %>% as.character, "<br>",
+                               "EIC Median:", x %>% extract2("data") %>% arrange(desc(EIC_median)) %>% extract2("EIC_median") %>% round(0)
+    )
+    
+    return(gg)
+}

0d5f1a32e83d5c28aa299bcaed66e31ec4a38643
Jan Stanstrup <stanstrup@gmail.com>
2016-07-26 13:46:31 +0200

    removed EIC summarise function and make example instead of how it can be easily done unnested.

--- Module_snippets/functions.R
+++ Module_snippets/functions.R
@@ -15,27 +15,3 @@ cont_screen_gg2plotly <- function(x){
     return(gg)
 }
 
-
-
-
-get_EICs <- function(tbl){
-    require(xcms)
-    require(purrr)
-    require(plyr)
-    
-    
-    tbl %>%     
-        mutate(EIC   = pmap(list(raw,mz_lower,mz_upper),   function(raw,lower,upper) rawEIC(raw, as.matrix(data.frame(mzmin=lower,mzmax=upper)))   )) %>% # get the EICs
-        mutate(EIC   = map(EIC,  ~ do.call(cbind.data.frame,.) %>% as.tbl  )) %>%     # EICs are lists. make nice data.frame
-        mutate(EIC   = map2(EIC,raw, function(EIC,raw) mutate(EIC,scan_rt = raw@scantime[EIC$scan]/60)   )) %>% # convert scans to retention times
-        mutate(EIC_median   = map_dbl(EIC,  ~ median(.$intensity)   )) %>%  # get the median intensity of each EIC
-        mutate(EIC_mean     = map_dbl(EIC,  ~ mean(.$intensity)   )) %>%    # get the median intensity of each EIC
-        mutate(EIC_sd       = map_dbl(EIC,  ~ sd(.$intensity)   )) %>%      # get the sd intensity of each EIC
-        mutate(EIC_max      = map_dbl(EIC,  ~ max(.$intensity)   )) %>%     # get the max intensity of each EIC
-        mutate(EIC_max      = map_dbl(EIC,  ~ max(.$intensity)   )) ->      # get the max intensity of each EIC
-        out
-    
-    return(out)
-    
-}
-

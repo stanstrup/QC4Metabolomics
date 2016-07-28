b30c9e2359c64524c45112ca8863cf4408895ff9
Jan Stanstrup <stanstrup@gmail.com>
2016-07-28 15:42:12 +0200

    moved contamination screening to a vignette

--- Module_snippets/_test_flow.R
+++
@@ -1,4 +0,0 @@
-source("functions.R",                  local = FALSE, print.eval = TRUE)
-source("read_settings.R",              local = FALSE, print.eval = TRUE)
-source("load_example_data.R",          local = FALSE, print.eval = TRUE)
-source("get_targeted_contaminant.R",   local = FALSE, print.eval = TRUE)

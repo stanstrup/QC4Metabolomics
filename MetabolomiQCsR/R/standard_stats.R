4c4d474e1e5bae59c41e58500de4f59671bc2acd
Jan Stanstrup <stanstrup@gmail.com>
2016-08-12 15:19:25 +0200

    Targeted peak picking and calculating peak stats.

---
+++ MetabolomiQCsR/R/standard_stats.R
@@ -0,0 +1,177 @@
+#' Convert a list of peaks (rt / m/z pairs) to a Region of Interest (ROI) list for use with \code{\link{findPeaks}}
+#'
+#' @param tbl \code{\link{tibble}} containing the columns "rt" and "mz". rt needs to be in seconds.
+#' @param raw \code{\link{xcmsRaw}} object to create ROI for. It needs to be a specific \code{\link{xcmsRaw}} to match retention times to scan nubmers.
+#' @param ppm ppm tolerance for the generated ROI.
+#' @param rt_tol Retention time tolerance (in sec!) for the generated ROI.
+#'
+#' @return List containing the ROIs. Each list contains mz, mzmin, mzmax, scmin, scmax, length (set to -1, not used by centWave) and intensity (set to -1, not used by centWave) columns.
+#' @export
+#' 
+#' @importFrom dplyr %>% rowwise transmute ungroup mutate
+#' @importFrom purrr by_row map
+#' @importFrom massageR attr_rem
+#' @importFrom magrittr extract2
+#' 
+
+tbl2ROI <- function(tbl, raw, ppm, rt_tol) {
+
+    rt <- mz <- .out <- NULL
+    
+    tbl %>% 
+             rowwise %>% 
+             transmute(mz        = mz, # not used!
+                       mzmin     = mz - ppm * mz * 1E-6, 
+                       mzmax     = mz + ppm * mz * 1E-6,
+                       scmin     = which.min(abs((rt-rt_tol)-raw@scantime)),
+                       scmax     = which.min(abs((rt+rt_tol)-raw@scantime)),
+                       length    = -1, # not used!
+                       intensity = -1  # not used!
+                       ) %>%
+             ungroup %>% 
+        
+             by_row(as.list) %>% 
+             mutate( .out = map(.out, ~ attr_rem(.x,"indices"))) %>% # we get some indices attribute. Dunno why. But lets remove it.
+             extract2(".out") ->
+    out
+    
+    return(out)
+}
+
+
+
+
+
+#' Match list of standards to peak table
+#'
+#' This function will match a table of standard compounds and a peak table by m/z and retention time.
+#' If there is more than one possible hit the highest intensity peak will be chosen.
+#'
+#' @param stds \code{\link{tibble}} of standards to match to a peak table
+#' @param peakTable \code{\link{tibble}} containing peak table supplied by \code{\link{findPeaks}} (but converted to \code{\link{tibble}}/\code{\link{data.frame}}).
+#' @param rt_tol Retention time tolerance for matching peaks. Pay attention to the unit of your tables.
+#' rt_tol should match and stds and peakTable should use same units (i.e. minutes of seconds).
+#' @param mz_ppm ppm for matching peaks.
+#' @param rt_col Character string giving the column containing the retention times. Must be same in standards and peak table.
+#' @param mz_col Character string giving the column containing the m/z values. Must be same in standards and peak table. 
+#' @param int_col Character string giving the column containing the intensities in the peak table. 
+#'
+#' @return A vector having the length equivalent to the number of rows in stds giving the indices of the hits in peakTable.
+#' 
+#' @export
+#'
+#' @importFrom magrittr extract2
+#' @importFrom dplyr %>% slice
+#'
+
+closest_match <- function(stds, peakTable, rt_tol = 0.25, mz_ppm = 30, rt_col = "rt", mz_col = "mz", int_col = "into"){
+    
+    indices <- rep_len(as.numeric(NA), nrow(stds))
+    
+    peakTable_mz <- peakTable %>% extract2(mz_col)
+    peakTable_rt <- peakTable %>% extract2(rt_col)
+        
+    for(i in 1:nrow(stds)){
+        
+        stds_mz <- stds %>% slice(i) %>% extract2(mz_col)
+        stds_rt <- stds %>% slice(i) %>% extract2(rt_col)
+        
+        
+        idx <-  abs(stds_rt-peakTable_rt) < rt_tol & 
+               (abs(stds_mz-peakTable_mz)/stds_mz)*1E6 <  mz_ppm
+        
+        # no match is found
+        if(sum(idx)==0){ next } # we filled the vector with NA so we don't need to do anyting
+        
+        # 1 match is found
+        if(sum(idx)==1){
+            indices[i] <- which(idx) 
+            next
+            }
+        
+        # More than one match is found. We take the highest intensity peak then
+        if(sum(idx)>1){
+            idx2       <- peakTable %>% slice(which(idx)) %>% extract2(int_col) %>% which.max
+            indices[i] <- which(idx)[idx2]
+            next
+            }
+            
+    }
+    
+    return(indices)
+}
+
+
+
+
+
+#' Calculate Tailing Factor and Asymmetry Factor
+#'
+#' @param EIC EIC containing the peak to calculate for. 
+#' \code{\link{tibble}} as produced with \code{\link{get_EICs}}.
+#' @param rt Retention time of the center of the peak (Numeric)
+#' @param factor to calculate. Character string either "TF" (Tailing Factor) or "ASF" (Asymmetry Factor).
+#'
+#' @references http://www.chromforum.org/viewtopic.php?t=20079
+#'
+#' @return Numeric
+#' @export
+#'
+#' @importFrom dplyr %>% filter arrange desc mutate slice
+#' @importFrom stats approx
+#'
+
+#'
+
+peak_factor <- function(EIC, rt, factor="TF"){
+
+    intensity <- per_max <- scan_rt <- NULL
+    
+    
+    # C = midpoint = highest scan
+    C <- rt/60 # EIC is in minutes so we change here
+    
+    C_scan <- EIC$scan[    which.min(abs(EIC$scan_rt-C))   ]
+    
+    C <- EIC %>% filter(scan==C_scan) %>% extract2("scan_rt")
+    
+    
+    # Get the lower RT side of the peak
+    A_side <-   EIC %>% 
+                filter(scan <= C_scan) %>% 
+                arrange(desc(scan)) %>% 
+                mutate(per_max = intensity/intensity[1])
+    
+    # Get the upper RT side of the peak
+    B_side <-   EIC %>% 
+                filter(scan >= C_scan) %>% 
+                arrange(scan) %>% 
+                mutate(per_max = intensity/intensity[1])
+    
+    
+    # which factor are we doing?
+    cut_off <- switch(factor, TF=0.05, ASF=0.1)
+    
+    
+    
+    # Get A
+    A_scans <- A_side %>% with(match(-1,sign(per_max-cut_off)))
+    
+    A       <- A_side %>% 
+               slice((A_scans-1):A_scans) %>% 
+               with(approx(per_max,scan_rt,xout=cut_off)$y)
+    
+    
+    # Get B
+    B_scans <- B_side %>% with(match(-1,sign(per_max-cut_off)))
+    
+    B       <- B_side %>% 
+               slice((B_scans-1):B_scans) %>% 
+               with(approx(per_max,scan_rt,xout=cut_off)$y)
+    
+    
+    
+    if(factor=="TF") return(   (B-A)/(2*(C-A))   )
+    
+    if(factor=="ASF") return(   (B-C)/(C-A)   )
+}
--- MetabolomiQCsR/inst/doc/standard_stats.R
+++ MetabolomiQCsR/inst/doc/standard_stats.R
@@ -12,58 +12,20 @@ library(listviewer) # https://github.com/timelyportfolio/listviewer
 library(purrr)
 library(magrittr)
 library(dplyr)
-
-## ----best match----------------------------------------------------------
-closest_match <- function(a,b, rt_tol, mz_ppm, rt_col = "rt", mz_col = "mz"){
-    
-    indices <- rep_len(as.numeric(NA), nrow(a))
-    
-    b_mz <- b %>% extract2(mz_col)
-    b_rt <- b %>% extract2(rt_col)
-        
-    for(i in 1:nrow(a)){
-        
-        a_mz <- a %>% slice(i) %>% extract2(mz_col)
-        a_rt <- a %>% slice(i) %>% extract2(rt_col)
-        
-        
-        idx <-  abs(a_rt-b_rt) < rt_tol & 
-               (abs(a_mz-b_mz)/a_mz)*1E6 <  mz_ppm
-        
-        # no match is found
-        if(sum(idx)==0){ next } # we filled the vector with NA so we don't need to do anyting
-        
-        # 1 match is found
-        if(sum(idx)==1){
-            indices[i] <- which(idx) 
-            next
-            }
-        
-        # More than one match is found. RT wil decide
-        if(sum(idx)>1){
-            idx2       <- which.min(abs(a_rt-b_rt[idx]))
-            indices[i] <- which(idx)[idx2]
-            next
-            }
-            
-    }
-    
-    return(indices)
-}
-
-
+library(massageR)
+library(tidyr)
 
 ## ----settings------------------------------------------------------------
 settings <- list()
 
 settings$xcmsRaw$profparam          <- list(step=0.005)
 
-settings$std_match$ppm                <- 20
+settings$std_match$ppm                <- 100 # this needs to be quite large for this strategy to work. Only scans inside this is used AFAIK. The centwave ppm setting will limit appropiately
 settings$std_match$rt_tol             <- 0.25*60
 
 settings$findPeaks$method             <- 'centWave'
 settings$findPeaks$snthr              <- 10
-settings$findPeaks$ppm                <- 20
+settings$findPeaks$ppm                <- 30
 settings$findPeaks$peakwidth          <- c(0.05*60,0.3*60)
 settings$findPeaks$scanrange          <- NULL
 settings$findPeaks$prefilter          <- c(3,1E3)
@@ -78,65 +40,107 @@ files <- c("Z:/_Data/LIP1/0036_SevaMeal/mzML/0036_LIP1p_20160201_034_2_1_1.mzML"
            "Z:/_Data/LIP1/0036_SevaMeal/mzML/0036_LIP1p_20160201_057_36_1_1.mzML"
            )
 
-## ------------------------------------------------------------------------
+## ----list of standards---------------------------------------------------
 stds <- read_tsv("stds.tsv")
 stds
-
-## ----read raw files------------------------------------------------------
-raw <- xcmsRaw_to_tbl(files, profparam = settings$xcmsRaw$profparam)
-
-raw
 
 ## ----md5-----------------------------------------------------------------
 raw %<>% mutate(md5 = path %>% as.character %>% md5sum %>% as.vector)
 
+## ----merge tables--------------------------------------------------------
+stds %>%    mutate(rt = rt*60) %>% 
+            list %>% 
+            rep(nrow(raw))%>% 
+            data_frame(stds=.) %>% 
+            bind_cols(raw,.) ->
+data
+
+rm(stds,raw)
+
 ## ----make ROIs-----------------------------------------------------------
-
-attr_rem <- function(x, which) {
-    attr(x, which) <- NULL
-    return(x)
-}
-
-settings$findPeaks$ROI.list <-   
-             stds %>% 
-             rowwise %>% 
-             transmute(mz        = mz, # not used!
-                       mzmin     = mz - settings$std_match$ppm * mz * 1E-6, 
-                       mzmax     = mz + settings$std_match$ppm * mz * 1E-6,
-                       scmin     = which.min(abs((rt*60-settings$std_match$rt_tol)-raw$raw[[1]]@scantime)),
-                       scmax     = which.min(abs((rt*60+settings$std_match$rt_tol)-raw$raw[[1]]@scantime)),
-                       length    = -1, # not used!
-                       intensity = -1  # not used!
-                       ) %>%
-             ungroup %>% 
-        
-             by_row(as.list) %>% 
-             mutate( .out = map(.out, ~ attr_rem(.x,"indices"))) %>% # we get some indices attribute. Dunno why. But lets remove it.
-             extract2(".out")
+data %<>%
+            mutate(ROI = map2(raw,stds, ~ tbl2ROI(.y, 
+                                            raw    = .x, 
+                                            ppm    = settings$std_match$ppm, 
+                                            rt_tol = settings$std_match$rt_tol
+                                            )
+                             )
+                   )
 
 
-settings$findPeaks$ROI.list[[7]]
+data$ROI[[1]][[7]]
 
 ## ----findPeaks-----------------------------------------------------------
 
-peakTable <- settings$findPeaks %>% 
-             lift_dl(findPeaks, object = raw$raw[[1]])() %>% 
-             as.data.frame %>% as.tbl %>% 
-             arrange(mz) %>% 
-             mutate_each(funs(./60),rt,rtmin,rtmax)
+findPeaks_l <- lift_dl(findPeaks) # trick to make findPeaks accept a list of arguments.
 
-peakTable %>% kable
+
+data %<>% mutate(peakTable = map2(raw, ROI, ~ findPeaks_l(settings$findPeaks, object = .x, ROI.list = .y) %>% 
+                                              as.data.frame %>% as.tbl
+                                  )
+                 )
+
+
+data$peakTable[[1]] %>% kable
+
+## ----matching peaks------------------------------------------------------
+
+data %<>% mutate(peakTable = map(peakTable, ~ mutate(.x, row = 1:nrow(.x)))) # we add a row index we can match by later
+             
+
+data %<>% mutate(std_peaks = map2(stds, peakTable, ~ closest_match(.x, .y, settings$std_match$rt_tol,
+                                                                  settings$std_match$ppm
+                                                                  ) %>% 
+                                                    data_frame(row = .) %>% 
+                                                    bind_cols(.x) %>% 
+                                                    left_join(.y, by = "row", suffix = c(".stds", ".peaks")) %>% 
+                                                    mutate(found = ifelse(is.na(row),FALSE,TRUE)) %>% 
+                                                    select(-row)
+                                 )
+                )
+
+
+
+data$std_peaks[[1]] %>% kable
+
+## ----approxfun-----------------------------------------------------------
+data %<>% mutate(scan2rt_fun = map(raw, ~ approxfun(seq_along(.x@scantime),.x@scantime)))
+
+## ----EIC-----------------------------------------------------------------
+data %<>% mutate(EIC = map2(raw,std_peaks,
+                           ~ get_EICs(.x, data_frame(mz_lower = .y$mz.stds - settings$findPeaks$ppm*.y$mz.stds*1E-6, 
+                                                     mz_upper = .y$mz.stds + settings$findPeaks$ppm*.y$mz.stds*1E-6)
+                                      )   
+                           )
+                )
+
+
+## ----flatten-------------------------------------------------------------
+data_flat <-    data %>% 
+                select(file,polarity,raw,md5,std_peaks,path,scan2rt_fun) %>% 
+                unnest(std_peaks, .drop = FALSE) %>% 
+                bind_cols(data_frame(EIC=unlist(data$EIC,recursive = FALSE))) # we cannot just unnest the EICs so we do it manally and re-add them
+
+data_flat %>% select(-scan2rt_fun, -raw, -EIC) %>% slice(1:12) %>% kable
+
+## ----deviations----------------------------------------------------------
+
+data_flat %<>% mutate(mz_dev = ((mz.peaks - mz.stds)/mz.stds)*1E6 ) %>% 
+               mutate(rt_dev = rt.peaks - rt.stds , rt_dev_min = rt_dev/60)
+
+## ----FWHM----------------------------------------------------------------
+data_flat %<>% rowwise %>% 
+               mutate(FWHM_scan  = 2*sqrt(2*log(2))*sigma) %>% 
+               mutate(FWHM_start = (scpos - FWHM_scan/2) %>% scan2rt_fun ) %>%
+               mutate(FWHM_end   = (scpos + FWHM_scan/2) %>% scan2rt_fun ) %>% 
+               mutate(FWHM       =  FWHM_end - FWHM_start ) %>%
+               mutate(FWHM_dp    =  scmax - scmin + 1 ) %>% # data points
+               ungroup %>% 
+               select(-FWHM_scan, -FWHM_start, -FWHM_end)
 
 ## ------------------------------------------------------------------------
-peakTable %<>% mutate(row = 1:n())
 
-stds %<>% closest_match(peakTable,settings$std_match$rt_tol/60,settings$std_match$ppm) %>% 
-         data_frame(row = .) %>% 
-         bind_cols(stds)
+data_flat %<>% mutate(TF =  map2_dbl(EIC,rt.peaks, ~ peak_factor(.x,.y,factor="TF"))) %>% 
+               mutate(ASF = map2_dbl(EIC,rt.peaks, ~ peak_factor(.x,.y,factor="ASF")))
 
-stds %<>% left_join(peakTable, by = "row", suffix = c(".stds", ".peaks")) %>% 
-          mutate(found = ifelse(is.na(row),FALSE,TRUE)) %>% 
-          select(-row)
-
-stds %>% kable
 

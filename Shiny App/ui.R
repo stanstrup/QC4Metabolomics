ca5312e0e510e86c5ca1a9778ee273b334ceabb2
Jan Stanstrup <stanstrup@gmail.com>
2017-02-02 11:24:17 +0100

    fixed the ui to work with modules.

--- Shiny App/ui.R
+++ Shiny App/ui.R
@@ -1,12 +1,14 @@
 
-shinyUI(           navbarPage("Navigation bar",
-                              
-                                                lapply(seq_along(module_names), 
+shinyUI(           
+                   
+                   do.call(navbarPage, c(list("Navigation bar"), 
+                                         
+                                         lapply(seq_along(module_names), 
                                                        function(i){ do.call(paste0(module_names[i],"UI"),list(id = paste0("name",i)))
                                                                   }
                                                        )
-
-                             )
-        
+                                         
+                                         
+                                         ))
                    
         )

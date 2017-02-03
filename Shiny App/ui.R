acf37e761ac3079411e28c77f565865e77e7125f
Jan Stanstrup <stanstrup@gmail.com>
2017-02-03 11:14:15 +0100

    changed navbar name

--- Shiny App/ui.R
+++ Shiny App/ui.R
@@ -1,5 +1,5 @@
 shinyUI(           
-        do.call(navbarPage, c(list("Navigation bar"), 
+        do.call(navbarPage, c(list("MetabolomiQCs"), 
                               lapply(seq_along(module_names), 
                                      function(i){    do.call(paste0(module_names[i],"UI"),list(id = paste0("name",i)))    }
                                      )

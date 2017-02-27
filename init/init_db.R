86e646f61758462ab238e1af068351669be7fde7
Jan Stanstrup <stanstrup@gmail.com>
2017-02-27 16:02:41 +0100

    SMALLINT to TINYINT

--- init/init_db.R
+++ init/init_db.R
@@ -58,7 +58,7 @@ sql <- c(sql, "
                 CREATE TABLE file_schedule (
                 file_md5      CHAR(32)       NOT NULL,
                 module        VARCHAR(256)   NOT NULL,
-                priority      SMALLINT       NOT NULL,
+                priority      TINYINT       NOT NULL,
 
                 FOREIGN KEY(file_md5) REFERENCES files(file_md5)
                 )

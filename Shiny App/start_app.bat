3b5d60f3e4690c60aa00c341415b7cb5da69c411
Jan Stanstrup <stanstrup@gmail.com>
2017-08-31 13:28:05 +0200

    made the startup script make shiny accessable from anywhere and on fixed port.

--- Shiny App/start_app.bat
+++ Shiny App/start_app.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\R\R-3.3.0\bin\R.exe" -e "shiny::runApp(launch.browser=TRUE)"
+"C:\Program Files\R\R-3.3.0\bin\R.exe" -e "shiny::runApp(host='0.0.0.0', port=3333, launch.browser=TRUE)"

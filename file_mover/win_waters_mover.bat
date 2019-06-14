ce0df0afbb409687d61858a30cce383e56af7668
Jan Stanstrup <stanstrup@gmail.com>
2019-06-14 15:30:39 +0200

    customizeable mover and checks filename format

--- file_mover/win_waters_mover.bat
+++ file_mover/win_waters_mover.bat
@@ -1,32 +1,58 @@
 @echo off
+
+REM ****** SETTINGS ******
 REM No trailing spaces!
 set "infolder=C:\Users\tmh331\Desktop\temp with spaces\in"
 set "outfolder=C:\Users\tmh331\Desktop\temp with spaces\out"
+set "delim=_"
+set "expect_delims=4"
 
 
+REM SCRIPT STARTS HERE
+setlocal enableDelayedExpansion
+set /a "token_start=%expect_delims%+1"
+set /a "token_end=%expect_delims%+2"
+	
+	
 REM Resursively look for folders with _extern.inf files
 for /d /r "%infolder%" %%i in (*) do  @if exist %%i\_extern.inf (
 
-	REM echo the raw folder is %%i
-
-	REM tokens decide which part to take of the split string.
-	REM "tokens=1,3" would take first and third and make %%b avaiable
-
-	for /F "tokens=1 delims=_" %%a in ("%%~ni") do (
-		
-		REM Create project folder if doens't exist
-		if not exist "%outfolder%\%%a.raw\Data\" (
-		echo creating folder %outfolder%\%%a.raw\Data
-		mkdir "%outfolder%\%%a.raw\Data"
-		)
-		
-		REM move raw folder if doesn't exist
-		if exist "%outfolder%\%%a.raw\Data\%%~nxi" echo raw folder already exists!
-		
-		if not exist "%outfolder%\%%a.raw\Data\%%~nxi" (
-		echo Moving "%%~fi" to "%outfolder%\%%a.raw\Data\%%~nxi"
-		move  "%%~fi" "%outfolder%\%%a.raw\Data\%%~nxi"
+	echo ****** Processing file "%%~i" ******
+	
+	REM export variables
+	for /f "delims=" %%B in ('set token_') do (
+		if "!"=="" endlocal
+		set "%%B"
+    )
+	
+	
+	for /F "tokens=1,%token_start%,%token_end% delims=%delim%" %%a in ("%%~ni") do (
+		REM echo the raw folder is %%i
+		if "%%b" == "" (echo Too few delimiters in "%%~xni") else (
+			if not "%%c" == "" (echo Too many delimiters in "%%~xni") else (
+			
+			
+				REM Create project folder if doesn't exist
+				if not exist "%outfolder%\%%a.raw\Data" (
+					echo creating folder "%outfolder%\%%a.raw\Data"
+					mkdir "%outfolder%\%%a.raw\Data"
+				)
+				
+				REM move raw folder if doesn't exist
+				if exist "%outfolder%\%%a.raw\Data\%%~nxi" echo raw folder already exists!
+				
+				if not exist "%outfolder%\%%a.raw\Data\%%~nxi\" (
+					echo Moving "%%~fi" to "%outfolder%\%%a.raw\Data\%%~nxi"
+					move  "%%~fi" "%outfolder%\%%a.raw\Data\%%~nxi"
+				)
+			
+				
+				
+				
+			)	 
 		)
 	)
+	
+	
 
 )

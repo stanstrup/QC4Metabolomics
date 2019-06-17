70449eacdc35ff9c7c87844e9e829bc910af983e
Jan Stanstrup <stanstrup@gmail.com>
2019-06-17 10:00:45 +0200

    removed unnecessary enableDelayedExpansion and made better debug prints.

--- file_mover/win_waters_mover.bat
+++ file_mover/win_waters_mover.bat
@@ -5,11 +5,10 @@ REM No trailing spaces!
 set "infolder=C:\Users\tmh331\Desktop\temp with spaces\in"
 set "outfolder=C:\Users\tmh331\Desktop\temp with spaces\out"
 set "delim=_"
-set "expect_delims=4"
+set "expect_delims=2"
 
 
 REM SCRIPT STARTS HERE
-setlocal enableDelayedExpansion
 set /a "token_start=%expect_delims%+1"
 set /a "token_end=%expect_delims%+2"
 	
@@ -17,21 +16,17 @@ set /a "token_end=%expect_delims%+2"
 REM Resursively look for folders with _extern.inf files
 for /d /r "%infolder%" %%i in (*) do  @if exist %%i\_extern.inf (
 
-	echo ****** Processing file "%%~i" ******
-	
-	REM export variables
-	for /f "delims=" %%B in ('set token_') do (
-		if "!"=="" endlocal
-		set "%%B"
-    )
-	
-	
+	echo ****** Start processing ******
+	echo File: "%%~i"
+
+
 	for /F "tokens=1,%token_start%,%token_end% delims=%delim%" %%a in ("%%~ni") do (
 		REM echo the raw folder is %%i
-		if "%%b" == "" (echo Too few delimiters in "%%~xni") else (
-			if not "%%c" == "" (echo Too many delimiters in "%%~xni") else (
+		if "%%b" == "" (echo Filename check: Too few delimiters. File ignored.) else (
+			if not "%%c" == "" (echo Filename check: Too many delimiters. File ignored.) else (
 			
-			
+				echo Filename check: OK
+				
 				REM Create project folder if doesn't exist
 				if not exist "%outfolder%\%%a.raw\Data" (
 					echo creating folder "%outfolder%\%%a.raw\Data"
@@ -39,18 +34,18 @@ for /d /r "%infolder%" %%i in (*) do  @if exist %%i\_extern.inf (
 				)
 				
 				REM move raw folder if doesn't exist
-				if exist "%outfolder%\%%a.raw\Data\%%~nxi" echo raw folder already exists!
+				if exist "%outfolder%\%%a.raw\Data\%%~nxi" echo raw folder already exists! Folder ignored.
 				
 				if not exist "%outfolder%\%%a.raw\Data\%%~nxi\" (
 					echo Moving "%%~fi" to "%outfolder%\%%a.raw\Data\%%~nxi"
 					move  "%%~fi" "%outfolder%\%%a.raw\Data\%%~nxi"
 				)
-			
-				
-				
 				
 			)	 
 		)
+		
+		echo ****** End processing ******
+		echo.
 	)
 	
 	

5953eda0a5e9e08b905cfd4b3a2a6ca5f0d75332
Jan Stanstrup <stanstrup@gmail.com>
2019-06-18 13:38:06 +0200

    added check to not touch folders that are symlinks

--- file_mover/win_waters_mover.bat
+++ file_mover/win_waters_mover.bat
@@ -10,51 +10,57 @@ set "symlinkback=TRUE"
 
 
 REM SCRIPT STARTS HERE
-setlocal
+setlocal enabledelayedexpansion
 set /a "token_start=%expect_delims%+1"
 set /a "token_end=%expect_delims%+2"
-	
 	
 REM Resursively look for folders with _extern.inf files
 for /d /r "%infolder%" %%i in (*) do  @if exist %%i\_extern.inf (
 
 	echo ****** Start processing ******
-	echo File: "%%~i"
+	echo File: "%%i"
+	
+	REM detect if a symlink and if so don't do anything more.
+	set "issymlink=FALSE"
+	fsutil reparsepoint query "%%i" | find "Symbolic Link" >nul && echo Symlink detected. Skipping. && set "issymlink=TRUE"
 
+	if not !issymlink! == TRUE (
+	
 
-	for /F "tokens=1,%token_start%,%token_end% delims=%delim%" %%a in ("%%~ni") do (
-		REM echo the raw folder is %%i
-		if "%%b" == "" (echo Filename check: Too few delimiters. File ignored.) else (
-			if not "%%c" == "" (echo Filename check: Too many delimiters. File ignored.) else (
-			
-				echo Filename check: OK
+		for /F "tokens=1,%token_start%,%token_end% delims=%delim%" %%a in ("%%~ni") do (
+			REM echo the raw folder is %%i
+			if "%%b" == "" (echo Filename check: Too few delimiters. File ignored.) else (
+				if not "%%c" == "" (echo Filename check: Too many delimiters. File ignored.) else (
 				
-				REM Create project folder if doesn't exist
-				if not exist "%outfolder%\%%a.raw\Data" (
-					echo creating folder "%outfolder%\%%a.raw\Data"
-					mkdir "%outfolder%\%%a.raw\Data"
-				)
-				
-				REM move raw folder if doesn't exist
-				if exist "%outfolder%\%%a.raw\Data\%%~nxi" echo raw folder already exists! Folder ignored.
-				
-				if not exist "%outfolder%\%%a.raw\Data\%%~nxi\" (
-					echo Moving "%%~fi" to "%outfolder%\%%a.raw\Data\%%~nxi"
-					move  "%%~fi" "%outfolder%\%%a.raw\Data\%%~nxi"
-					
-					REM make symlink in original location
-					if %symlinkback% == TRUE (
-						mklink /D "%%~fi" "%outfolder%\%%a.raw\Data\%%~nxi"
+					echo Filename check: OK
+
+					REM Create project folder if doesn't exist
+					if not exist "%outfolder%\%%a.raw\Data" (
+						echo creating folder "%outfolder%\%%a.raw\Data"
+						mkdir "%outfolder%\%%a.raw\Data"
 					)
-				)
-				
-			)	 
+					
+					REM move raw folder if doesn't exist
+					if exist "%outfolder%\%%a.raw\Data\%%~nxi" echo raw folder already exists! Folder ignored.
+					
+					if not exist "%outfolder%\%%a.raw\Data\%%~nxi\" (
+						echo Moving "%%~fi" to "%outfolder%\%%a.raw\Data\%%~nxi"
+						move  "%%~fi" "%outfolder%\%%a.raw\Data\%%~nxi"
+						
+						REM make symlink in original location
+						if %symlinkback% == TRUE (
+							mklink /D "%%~fi" "%outfolder%\%%a.raw\Data\%%~nxi"
+						)
+					)
+
+				)	 
+			)
 		)
-		
-		echo ****** End processing ******
-		echo.
+
+
 	)
-	
-	
+
+	echo ******* End processing *******
+	echo.
 
 )

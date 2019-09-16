e0ed9d06c40cc2cf960019d6b82ea9927c1343f2
Jan Stanstrup <stanstrup@gmail.com>
2019-09-16 14:49:16 +0200

    write output files instead of input raw

--- setup/converter_std.R
+++ setup/converter_std.R
@@ -11,9 +11,13 @@ outdir <- paste0(dirname(files), Sys.getenv("msconvert_outdir_prefix"))
 # remove files that already exist
 files_b <- basename(files)
 files_out <- paste0(outdir,"/",gsub(".raw$","",files_b),".mzML")
+
 file_exist <-  file.exists(files_out)
+
 files <- files[!file_exist]
 outdir <- outdir[!file_exist]
+files_out <- files_out[!file_exist]
+
 
 
 # if there are new files do the conversion
@@ -21,7 +25,7 @@ if(length(files)!=0){
   
   sapply(unique(outdir), function(x) dir.create(x, recursive = TRUE, showWarnings = FALSE))
   
-  cmd <- paste0('wine msconvert "',files,'" ', Sys.getenv("msconvert_args"), ' --outdir "',outdir,'" && echo "',files,'" >> "',basedir,'"/mzML_filelist.txt')
+  cmd <- paste0('wine msconvert "',files,'" ', Sys.getenv("msconvert_args"), ' --outdir "',outdir,'" && echo "',files_out,'" >> "',basedir,'"/mzML_filelist.txt')
   
   sapply(cmd, system)
   

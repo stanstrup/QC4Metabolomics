76c55de482c63d2ec63869a204fd10044d2aae4f
Jan Stanstrup <stanstrup@gmail.com>
2019-09-17 15:24:41 +0200

    start cron with environments also in the processor

--- setup/scheduled_tasks.sh
+++ setup/scheduled_tasks.sh
@@ -2,4 +2,8 @@
 
 [ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -n "$0" "$0" "$@" || :
 
+# import env that was saved before starting cron
+. $HOME/env.sh
+
+# Start R script
 /usr/local/bin/Rscript '/srv/shiny-server/QC4Metabolomics/setup/scheduled_tasks.R'

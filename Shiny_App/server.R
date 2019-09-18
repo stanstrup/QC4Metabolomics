438090c07b1deba5271427729f0f3cb18fe93a72
Jan Stanstrup <stanstrup@gmail.com>
2019-09-18 17:07:44 +0200

    Selection of instrument inserted

--- Shiny_App/server.R
+++ Shiny_App/server.R
@@ -1,5 +1,28 @@
 shinyServer(function(input, output, session) {
 
+	# Get available instrument
+	global_instruments_available <-    reactive({    "
+													 SELECT DISTINCT instrument
+													 FROM file_info
+													" %>% 
+													dbGetQuery(pool,.) %>% 
+													as.matrix %>% as.character
+											   })
+
+
+	# build the ui
+	output$global_instruments_select_ui <- renderUI({
+													 
+													 selectInput("global_instruments_input", 
+																   label = "Instruments", 
+																   choices  = global_instruments_available(),
+																   selected = global_instruments_available(),
+																   multiple = TRUE
+																 )
+
+	})
+
+
     # Modules
     lapply(seq_along(module_names), 
            function(i){ callModule(get(module_names[i]), paste0("name",i))
--- Shiny_App/ui.R
+++ Shiny_App/ui.R
@@ -1,5 +1,16 @@
+#from https://stackoverflow.com/questions/40604394/search-field-in-shiny-navbarpage
+navbarPageWithInputs <- function(..., inputs) {
+  navbar <- navbarPage(...)
+  form <- tags$form(class = "navbar-form", style="float: right;", inputs)
+  navbar[[3]][[1]]$children[[1]] <- htmltools::tagAppendChild(
+    navbar[[3]][[1]]$children[[1]], form)
+  navbar
+}
+
+ 
 shinyUI(           
-        do.call(navbarPage, c(list("MetabolomiQCs"), 
+        do.call(navbarPageWithInputs, c(list("MetabolomiQCs"),
+							  inputs=list(uiOutput("global_instruments_select_ui")),
                               lapply(seq_along(module_names), 
                                      function(i){    do.call(paste0(module_names[i],"UI"),list(id = paste0("name",i)))    }
                                      ) %>% unlist(recursive=FALSE)

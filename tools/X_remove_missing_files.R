9b059bc0557bdf766dfee03d2da8f0876f23087d
Jan Stanstrup <stanstrup@gmail.com>
2022-02-01 10:36:44 +0100

    script to remove missing files

---
+++ tools/X_remove_missing_files.R
@@ -0,0 +1,50 @@
+# Libraries ---------------------------------------------------------------
+library(MetabolomiQCsR)
+library(dplyr)
+library(magrittr)
+#library(stringr)
+library(DBI)
+library(RMySQL)
+library(pool) # devtools::install_github("rstudio/pool")   
+library(xml2)
+library(purrr)
+library(tidyr)
+library(fs)
+
+
+# Establish connection to get new files -----------------------------------------------
+pool <- dbPool_MetabolomiQCs(30)
+
+
+
+
+file_tbl <- "SELECT * FROM files" %>% 
+                dbGetQuery(pool,.) %>% 
+                as_tibble %>% 
+                mutate(path = paste0(MetabolomiQCsR.env$general$base,"/",path))
+
+file_tbl %<>% mutate(file_found = file_exists(path))
+
+
+bad_file_md5 <- file_tbl %>% filter(!file_found) %>% pull(file_md5)
+
+
+tables_to_prune <- c("std_stat_data", "file_schedule", "file_info", "cont_data", "files")
+
+
+cmd <- expand.grid(bad_file_md5, tables_to_prune, stringsAsFactors = FALSE)
+
+
+sql_query <- paste0("DELETE FROM ",cmd$Var2," WHERE file_md5='",cmd$Var1,"';")
+
+
+con <- poolCheckout(pool)
+      dbBegin(con)
+      
+      
+for(i in seq_along(sql_query)){
+  dbSendQuery(con,sql_query[i])
+  dbCommit(con)
+}
+
+poolReturn(con)

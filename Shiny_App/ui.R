3da40eb79e721e85e16331deda890ccf84724662
Jan Stanstrup <stanstrup@gmail.com>
2024-07-25 15:45:21 +0200

    updated to new navbarPage structure

--- Shiny_App/ui.R
+++ Shiny_App/ui.R
@@ -2,12 +2,15 @@
 navbarPageWithInputs <- function(..., inputs) {
   navbar <- navbarPage(...)
   form <- tags$form(class = "navbar-form", style="float: right; margin-bottom: 0px;", inputs)
-  navbar[[4]][[1]][[1]]$children[[1]]$children[[2]] <- htmltools::tagAppendChild(navbar[[4]][[1]][[1]]$children[[1]]$children[[2]], form)
+  
+  navbar[[4]][[1]]$children[[1]][[1]]$children[[1]]$children[[2]] <- htmltools::tagAppendChild(navbar[[4]][[1]]$children[[1]][[1]]$children[[1]]$children[[2]], form)
   navbar
 }
 
 
-css <- tags$head(
+tagList(
+
+tags$head(
   tags$style(type="text/css", 
                "
                 #inline label{ display: table-cell; text-align: center; vertical-align: middle; } 
@@ -15,16 +18,15 @@ css <- tags$head(
                 #inline .selectize-control { width: 200px; margin: 0;}
                "
              )
-)
+),
 
           
 do.call(navbarPageWithInputs, c(list("MetabolomiQCs"),
 			                          inputs=list(uiOutput("global_instruments_select_ui")),
                                 lapply(seq_along(module_names), 
                                        function(i){do.call(paste0(module_names[i],"UI"),list(id = paste0("name",i)))}
-                                      ) %>% 
-			                          unlist(recursive=FALSE) %>% c(., list(css))
+                                      )
                               )
         )
                    
-
+)

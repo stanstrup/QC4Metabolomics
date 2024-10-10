a35b28f7f3051a3101a9de0b90cdd06bf7e1f807
Jan Stanstrup <stanstrup@gmail.com>
2024-10-10 15:14:00 +0200

    allow only one row per file / module combination

--- Modules/FileSchedule/init_db_create.sql
+++ Modules/FileSchedule/init_db_create.sql
@@ -2,7 +2,8 @@ CREATE TABLE file_schedule (
 file_md5      CHAR(32)       NOT NULL,
 module        VARCHAR(256)   NOT NULL,
 priority      TINYINT       NOT NULL,
-FOREIGN KEY(file_md5) REFERENCES files(file_md5)
+FOREIGN KEY(file_md5) REFERENCES files(file_md5),
+PRIMARY KEY(file_md5, module)
 );
 
 

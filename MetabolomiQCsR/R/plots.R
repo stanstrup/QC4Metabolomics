daedec5c6b20a32fa858d62d6342321cbec01150
Jan Stanstrup <stanstrup@gmail.com>
2026-08-31 14:47:20 +0200

    fix: replace deprecated aes_string() and size= in plots.R (M12)

--- MetabolomiQCsR/R/plots.R
+++ MetabolomiQCsR/R/plots.R
@@ -8,17 +8,18 @@
 #' @return a \code{\link{ggplot}} object.
 #' @export
 #' 
-#' @importFrom ggplot2 ggplot aes_string geom_line geom_hline
+#' @importFrom ggplot2 ggplot aes geom_line geom_hline
 #' @importFrom ggplot2 labs scale_y_continuous expand_limits
 #' @importFrom ggplot2 theme theme_bw element_blank element_line element_text
 #' @importFrom scales scientific
+#' @importFrom rlang .data
 #'
-#' 
+#'
 
 plot_chrom <- function(tbl, RT_col = "RT", Intensity_col = "Intensity"){
-    
-    ggplot(data=tbl,aes_string(x = RT_col, y = Intensity_col)) + 
-        geom_line(size=0.2) +
+
+    ggplot(data=tbl, aes(x = .data[[RT_col]], y = .data[[Intensity_col]])) +
+        geom_line(linewidth=0.2) +
         theme_bw() +
         theme(
             plot.background = element_blank()
@@ -27,14 +28,14 @@ plot_chrom <- function(tbl, RT_col = "RT", Intensity_col = "Intensity"){
             ,panel.border = element_blank()
         ) +
         #draws x and y axis line
-        theme(axis.line.x = element_line(color = "black", size = 0.5),
-              axis.line.y = element_line(color = "black", size = 0.5)) + 
+        theme(axis.line.x = element_line(color = "black", linewidth = 0.5),
+              axis.line.y = element_line(color = "black", linewidth = 0.5)) +
         labs(x="Retention time (min)", y="Intensity (counts)") +
         theme(axis.title = element_text(size = 16,face = "bold")) +
         scale_y_continuous(labels = scientific) +
         expand_limits(y = 0) +
-        geom_hline(yintercept = 0, size = 0.5)
-    
+        geom_hline(yintercept = 0, linewidth = 0.5)
+
 }
 
 
@@ -51,23 +52,24 @@ plot_chrom <- function(tbl, RT_col = "RT", Intensity_col = "Intensity"){
 #' @return a \code{\link{ggplot}} object.
 #' @export
 #' 
-#' @importFrom ggplot2 ggplot aes_string geom_bar
+#' @importFrom ggplot2 ggplot aes geom_bar
 #' @importFrom ggplot2 ggtitle margin labs scale_y_continuous
 #' @importFrom ggplot2 theme_classic theme element_text element_blank
 #' @importFrom scales scientific
 #' @importFrom magrittr extract2
 #' @importFrom dplyr slice %>%
+#' @importFrom rlang .data
 #'
 
 plot_contaminants <- function(data, title, x_var = "comp_name", y_var = "EIC_median", y_lab = "Median EIC"){
-    
+
                         sort_ord <- data %>% extract2(y_var) %>% order(decreasing = TRUE)
-                        data[, x_var] <- factor(data %>% extract2(x_var) %>% as.character , 
+                        data[, x_var] <- factor(data %>% extract2(x_var) %>% as.character ,
                                                 data %>% slice(sort_ord) %>% extract2(x_var) %>% as.character %>% unique
                                                 )
-    
-                        
-                        ggplot(data=data, aes_string(x = x_var, y = y_var)) + 
+
+
+                        ggplot(data=data, aes(x = .data[[x_var]], y = .data[[y_var]])) +
                         geom_bar(stat = "identity",fill="black") +
                         theme_classic() +
                         theme(axis.text.x = element_text(hjust=1,size = 10, angle = 90, vjust = 0.5)) +

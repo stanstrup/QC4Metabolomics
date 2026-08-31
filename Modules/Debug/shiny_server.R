f90e56674fc34fe2eb84cdbde07b837de7db8ecd
Jan Stanstrup <stanstrup@gmail.com>
2026-08-31 14:47:39 +0200

    fix: remove plyr dependency, replace unrowname with as.data.frame(row.names=NULL) (L5)

--- Modules/Debug/shiny_server.R
+++ Modules/Debug/shiny_server.R
@@ -1,31 +1,28 @@
 Debug <- function(input, output, session, global_instruments_input){
     require(DT)
-    require(plyr)
     require(dplyr)
 
     output$wd <- renderPrint( getwd() )
-    
-    
+
+
     output$sessionInfo <- renderPrint( sessionInfo() )
-    
-    
+
+
     output$packages_packrat <- renderDataTable(
                                                 datatable({
-                                                    installed.packages() %>% 
-                                                                            unrowname %>% 
-                                                                            as.data.frame %>% 
-                                                                            select(Package,LibPath,Version,Built) %>% 
+                                                    installed.packages() %>%
+                                                                            as.data.frame(row.names = NULL) %>%
+                                                                            select(Package,LibPath,Version,Built) %>%
                                                                             filter(grepl("renv/library/",.$LibPath,fixed = TRUE))
                                                          })
                                               )
-    
-    
+
+
     output$packages <- renderDataTable(
                                         datatable({
-                                                    installed.packages() %>% 
-                                                                            unrowname %>% 
-                                                                            as.data.frame %>% 
-                                                                            select(Package,LibPath,Version,Built) %>% 
+                                                    installed.packages() %>%
+                                                                            as.data.frame(row.names = NULL) %>%
+                                                                            select(Package,LibPath,Version,Built) %>%
                                                                             filter(!grepl("renv/library/",.$LibPath,fixed = TRUE))
                                                   })
                                       )
